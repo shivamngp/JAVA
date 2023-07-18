d14b59adf8a6166862eaf75e3e4d91ea0d4079ed
shivamngp <114685459+shivamngp@users.noreply.github.com>
2023-07-19 01:29:05 +0530

    Update pairs.java

--- pairs.java
+++ pairs.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class pairs {
     public static void pp(int numbers[]) {
-        int tp=0;
+        int tp=0; // to calculate total no of pairs
         for (int i=0;i<numbers.length;i++){
             int curr = numbers[i];
             for (int j=i+1;j<numbers.length;j++) {
@@ -10,7 +10,7 @@ public class pairs {
             }
             System.out.println();
         }
-        System.out.println("total no of pairs = " +tp);
+        System.out.println("total no of pairs = " +tp); // printing total no of pairs
     }
     public static void main(String args[]){
         int numbers[]={1,5,6,7,9,5};

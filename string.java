4a67b981b00152f5ac7f2a61b3d7951ec6c71491
shivamngp <114685459+shivamngp@users.noreply.github.com>
2023-07-18 12:54:29 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,10 @@
+//package com.shivam;
+import java.util.Scanner;
+public class string {
+    public static void main(String[] args) {
+        String name = new String ("Shiivam");
+        String Name = "Shiivam";
+        System.out.print("My name is ");
+        System.out.println(name);
+    }    
+}

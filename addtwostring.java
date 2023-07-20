cbbf5dc4167c6e817030c3240aa5a61ee7e39560
shivamngp <114685459+shivamngp@users.noreply.github.com>
2023-07-20 20:36:48 +0530

    Add files via upload

---
+++ addtwostring.java
@@ -0,0 +1,10 @@
+//concatination of two string
+public class addtwostring {
+    public static void main(String args[]){
+    String str = "shivam";
+    String str2 = "raj";
+    String str3 = "Singh";
+    String str4 = str + " " + str2 + " " + str3;
+    System.out.println(str4);
+}
+}

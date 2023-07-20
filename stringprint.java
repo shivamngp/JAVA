c8415c27eac00119ab616662ff35022c2a5525cd
shivamngp <114685459+shivamngp@users.noreply.github.com>
2023-07-20 09:02:50 +0530

    Add files via upload

---
+++ stringprint.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class stringprint {
+    public static void main(String args[]){
+        char arr[]={'a','s','d','f'};
+        String str ="abcd";
+        String str2 = new String("fdhbfvd");
+       //System.out.println(arr);
+       //System.out.println(str);
+       //System.out.println(str2);
+              // taking input string from user........
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("enter the string : ");
+        name = sc.nextLine();
+        System.out.println("String entered by you is : ");
+        System.out.println(name);
+    }
+}

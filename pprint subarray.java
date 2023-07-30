df9973a463c9ada9e30a797494bfd91f545af496
shivamngp <114685459+shivamngp@users.noreply.github.com>
2023-07-30 08:22:16 +0530

    Add files via upload

---
+++ pprint subarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class pprintsubarray {
+    public static void printsubarray(int numbers[]){
+        for(int i=0;i<number.length;i++) {
+            int start =i;
+            for (int j=i+1;j<numbers.length;j++){
+                int end j;
+                for (int k=start;k<=end;k++){
+                    System.out.print(numbers[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public Static void main(String args[]) {
+        int numbers[]={5,6,4,8,9,85,63,24};
+        printsubarray(numbers);
+    }
+    }
